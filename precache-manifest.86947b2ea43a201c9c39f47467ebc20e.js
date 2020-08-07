ec249489fb0fb47fdc67d16bbf80d98995810999
Marcos Imfeld <sebastianimfeld@gmail.com>
2020-08-07 15:53:26 -0300

    Updates

---
+++ precache-manifest.86947b2ea43a201c9c39f47467ebc20e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e061f392b0b2cf817354ccd2e927fdee",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "d921bd019a662ba02038",
+    "url": "/react-tutorial/static/css/main.41933746.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d921bd019a662ba02038",
+    "url": "/react-tutorial/static/js/main.2e9114a5.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
